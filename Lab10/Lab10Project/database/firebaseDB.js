de4f956d107f8c38514aab62b9ed6d47dbb241d1
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2023-10-10 01:54:56 +0700

    lab10

--- Lab10/Lab10Project/database/firebaseDB.js
+++ Lab10/Lab10Project/database/firebaseDB.js
@@ -1,16 +1,11 @@
 import * as firebase from "firebase/compat";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAqGvfMd_HO0m9PaNZDunV4Fwj3HvHiyy0",
-  authDomain: "mobile-firebase-c0fe2.firebaseapp.com",
-  projectId: "mobile-firebase-c0fe2",
-  storageBucket: "mobile-firebase-c0fe2.appspot.com",
-  messagingSenderId: "313285591174",
-  appId: "1:313285591174:web:02658fed6b65c68ad91107",
-  measurementId: "G-K1SVEQM8NQ"
+  apiKey: "AIzaSyD-A83ljhq2bxcgN0L_rc_kf8oc7d3CT6s",
+  authDomain: "mobilelab10-5ddd8.firebaseapp.com",
+  projectId: "mobilelab10-5ddd8",
+  storageBucket: "mobilelab10-5ddd8.appspot.com",
+  messagingSenderId: "6973993925",
+  appId: "1:6973993925:web:e10c97c6a6671e3ca1d1ac"
 };
-
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 export default firebase;
